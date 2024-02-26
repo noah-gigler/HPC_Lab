90f174d9f2accefa574b35af0a8329780dcdf97f
Noah Gigler <ngigler@student.ethz.ch>
2024-02-26 15:26:23 +0100

    report

--- Project_1a/code/hostname.cpp
+++ Project_1a/code/hostname.cpp
@@ -6,6 +6,5 @@ int main() {
     char hostname[HOST_NAME_MAX];
     gethostname(hostname, HOST_NAME_MAX);
     std::cout << "Hostname: " << hostname << std::endl;
-    std::cout << "hi"<< std::endl;
     return 0;
 }
