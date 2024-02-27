1f69067c1f56b37b8adef34a456143f4a4e8b235
Noah Gigler <ngigler@eu-login-47.euler.ethz.ch>
2024-02-27 15:57:02 +0100

    stream benchmark

--- Project_1a/code/stream/stream_clean.sh
+++ Project_1a/code/stream/stream_clean.sh
@@ -6,17 +6,21 @@
 #SBATCH --cpus-per-task=1             # Number of CPUs per task
 #SBATCH --mem-per-cpu=2048            # Memory per CPU
 #SBATCH --time=00:01:00               # Wall clock time limit
-#SBATCH --constraint=EPYC_7H12
+#SBATCH --constraint=EPYC_7763
+CPU_TYPE="EPYC_7763"
 
+lscpu | grep "Model name" > output_"$CPU_TYPE".txt # get CPU model
+
+MEMORY="20000000"
+echo "Memory = $MEMORY" >> output_"$CPU_TYPE".txt 
 
 module load gcc # load a compiler (gcc/11.4.0)
-
 #32MB/8bytes * 4 = 16M
-gcc -O3 -march=native -DSTREAM_TYPE=double -DSTREAM_ARRAY_SIZE=20000000 \
+gcc -O3 -march=native -DSTREAM_TYPE=double -DSTREAM_ARRAY_SIZE=$MEMORY \
 -DNTIMES=20 stream.c -o stream_c.exe # compile
 
 # get job id
-job_id=$(sbatch --mem-per-cpu=2G --wrap "./stream_c.exe > slurm_output.txt" | awk '{print $4}')
+job_id=$(sbatch --mem-per-cpu=2G --wrap "./stream_c.exe > slurm_output_"$CPU_TYPE".txt" | awk '{print $4}')
 
 # wait until finished
 while squeue -j $job_id | grep -q $job_id
@@ -25,7 +29,7 @@ do
 done
 
 # get relevant results
-grep -E "Copy:|Scale:|Add:|Triad:|Function" slurm_output.txt > output.txt
+grep -E "Copy:|Scale:|Add:|Triad:|Function" slurm_output_"$CPU_TYPE".txt >> output_"$CPU_TYPE".txt
 
 # clean up
 rm slurm_stream.err slurm_stream.out temp.txt stream_c.exe slurm-"$job_id".out
