993b9fca4e754db8970db69d8043c94e68db77a2
Noah Gigler <ngigler@eu-login-44.euler.ethz.ch>
2024-03-23 16:41:36 +0100

    mandel

---
+++ project_2/code/mandel/mandel.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --job-name=mandel  # Job name    (default: sbatch)
+#SBATCH --output=mandel.out   # Output file (default: slurm-%j.out)
+#SBATCH --error=mandel.err    # Error file  (default: slurm-%j.out)
+#SBATCH --ntasks=1                # Number of tasks
+#SBATCH --constraint=EPYC_7763    # Select node with CPU
+#SBATCH --cpus-per-task=128       # Number of CPUs per task
+#SBATCH --mem-per-cpu=1024        # Memory per CPU
+#SBATCH --time=01:00:00           # Wall clock time limit
+
+# Load some modules
+module load gcc
+module load python/3.11.6
+
+module list
+
+# Compile
+gcc -o mandel mandel_par.c pngwriter.c walltime.c -lpng -fopenmp
+
+./mandel
