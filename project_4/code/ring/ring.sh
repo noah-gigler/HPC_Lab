b7e18e3bbcbfb29ee86f664cae2af1b1a2d1a06c
Noah Gigler <ngigler@ethz.ch>
2024-04-22 17:08:21 +0200

    not working ring

---
+++ project_4/code/ring/ring.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --job-name=ring_sum      # Job name    (default: sbatch)
+#SBATCH --output=ring_sum-%j.out # Output file (default: slurm-%j.out)
+#SBATCH --error=ring_sum-%j.err  # Error file  (default: slurm-%j.out)
+#SBATCH --nodes=5                 # Number of nodes
+#SBATCH --ntasks=5                # Number of tasks
+#SBATCH --ntasks-per-node=1       # Number of tasks per node
+#SBATCH --constraint=EPYC_7763    # Select node with CPU
+#SBATCH --cpus-per-task=1         # Number of CPUs per task
+#SBATCH --mem-per-cpu=1024        # Memory per CPU
+#SBATCH --time=00:05:00           # Wall clock time limit
+
+# Load some modules & list loaded modules
+module load gcc openmpi
+module list
+
+# Compile
+make clean
+make
+
+# Run the program with 4 MPI processes on 4 different nodes
+mpirun ./ring_sum
--- project_4/code/ring/ring_sum.c
+++ project_4/code/ring/ring_sum.c
@@ -9,8 +9,23 @@ int main(int argc, char *argv[]) {
   MPI_Comm_size(MPI_COMM_WORLD, &size);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-  // IMPLEMENT: Ring sum algorithm
   int sum = 0; // initialize sum
+  printf("Process %i: Sum = %i\n", rank, sum);
+  int next = (rank + 1) % size;
+  int prev = (rank - 1) % size;
+
+  MPI_Status status;
+  int p_rank = rank;
+  int n_rank;
+
+  for(int i = 0; i < size; ++i) {
+    MPI_Send(&p_rank, 1, MPI_INT, next, 0, MPI_COMM_WORLD);
+    MPI_Recv(&n_rank, 1, MPI_INT, next, 0, MPI_COMM_WORLD, &status);
+
+    sum += n_rank;
+    p_rank = n_rank;
+  }
+
   printf("Process %i: Sum = %i\n", rank, sum);
 
   // Finalize MPI
